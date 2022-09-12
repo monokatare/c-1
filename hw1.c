591387ed6729c5136da180fe62fad6347e24065d
monokatare <112602008+monokatare@users.noreply.github.com>
2022-09-12 13:33:16 +0900

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,12 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+int main()
+{
+	int in_1, in_2;
+	printf("input two int:");
+	scanf("%d %d", &in_1, &in_2);
+	printf("%d & %d= %d\n", in_1, in_2, in_1 & in_2);
+	printf("%d | %d= %d\n", in_1, in_2, in_1 | in_2);
+	printf("%d & %d= %d\n", in_1, in_2, in_1 ^ in_2);
+
+}
