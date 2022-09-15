b9bcbfd74e3cab3c32e6e63e417a1e8ea908d967
monokatare <112602008+monokatare@users.noreply.github.com>
2022-09-15 13:56:30 +0900

    Add files via upload

---
+++ hw2.c
@@ -0,0 +1,12 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+int main()
+{
+	double km;
+	double mi;
+	printf("km 입력:");
+	scanf("%lf", &km);
+	mi = km / 1.609;
+	printf("\n%.1f km is equal to %.1f miles", km, mi);
+	return 0;
+}
