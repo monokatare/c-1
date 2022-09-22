ed3708be5b5df15ae7322a3d6803c9f9883eb781
monokatare <112602008+monokatare@users.noreply.github.com>
2022-09-22 19:52:43 +0900

    Add files via upload

---
+++ hw3.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+#include <stdlib.h>
+int main()
+{
+	int s;
+	scanf("%d", &s);
+	for (int i = 0; i < s; i++) {
+		for (int j = i; j<s-1; j++) {
+			printf(" ");
+		}
+		for (int j = 0; j<=i*2; j++) {
+			printf("*");
+
+		}
+		printf("\n");
+
+	}
+}
