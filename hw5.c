d07ad1a0743f8384f05f0116ecb8dde5f7373051
monokatare <112602008+monokatare@users.noreply.github.com>
2022-10-06 14:15:27 +0900

    Add files via upload

---
+++ hw5.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+#include <stdlib.h>
+void binary(int num) {
+	if (num == 0)
+		return;
+	int k = num % 2;
+	binary(num / 2);
+	printf("%d", k);
+}
+
+int main(void)
+{
+	int c;
+	scanf("%d", &c);
+	binary(c);
+	return 0;
+}
